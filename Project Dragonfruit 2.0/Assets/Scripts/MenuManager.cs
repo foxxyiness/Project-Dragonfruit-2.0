d79b22e5fa959b5cc96a4e70b264b86b5dff744b
RyguyRockz <78097100+RyguyRockz@users.noreply.github.com>
2022-01-24 17:09:45 -0500

    main menu buttons

--- Project Dragonfruit 2.0/Assets/Scripts/MenuManager.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/MenuManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class MenuManager : MonoBehaviour
 {
-    public void playGame(string TestingScenePrefab)
+    public void playGame()
     {
-        SceneManager.LoadScene(TestingScenePrefab);
+        SceneManager.LoadScene(1);
+        Debug.Log("scene load");
     }
 
 
     public void ExitGame()
     {
         Application.Quit();
+        Debug.Log("quit");
     }
 }
