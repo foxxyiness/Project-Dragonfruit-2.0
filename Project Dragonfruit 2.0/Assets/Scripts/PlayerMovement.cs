a3defcba25deaef58d318587abe56d07f0428062
foxxyiness <coachfoxjp@gmail.com>
2022-01-31 16:09:38 -0500

    LIGHT
    lightfall ha destiny

--- Project Dragonfruit 2.0/Assets/Scripts/PlayerMovement.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Light Stuff")]
+    public GameObject latern;
+    public Sprite laternSprite;
+    [Header("Crouch Stuff")]
+    public SpriteRenderer SpriteRenderer;
+    public Sprite standing;
+    public Sprite crouching;
+    public CapsuleCollider2D Collider;
+    public Vector2 StandingSize;
+    public Vector2 CrouchingSize;
+   
     [Header("Player Speed and Stuff")]
     public float speed = 200f;
     public float movement;
     public int powerJump = 200;
     public float sprintSpeed;
-    [Header("Other Bools")]
+
+    [Header("Impotant Bools")]
     public bool faceRight;
+    public bool isCrouching;
     public bool isGrounded;
     public bool isSprinting;
+    public bool toggleLight;
 
+   
     public void PlayerMove()
     {
+        Collider.size = StandingSize;
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = standing;
         movement = Input.GetAxis("Horizontal");
         if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
@@ -44,6 +62,36 @@ public class PlayerMovement : MonoBehaviour
 
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed * Time.deltaTime, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 
+        //Crouching
+        if(Input.GetButton("Crouch") && (isSprinting == false && isGrounded == true))
+        {
+            LightOff();
+            SpriteRenderer.sprite = crouching;
+            Collider.size = CrouchingSize;
+            isCrouching = true;
+        }
+        else
+        {
+            SpriteRenderer.sprite = standing;
+            Collider.size = StandingSize;
+            isCrouching = false;
+        } 
+
+        //Enabling Light
+        if(Input.GetButtonDown("Light") && (toggleLight == false && isCrouching == false))
+        {
+            LightOn();
+        }
+        else if(Input.GetButtonDown("Light") && (toggleLight == true && isCrouching == false))
+        {
+            LightOff();
+            SpriteRenderer.sprite = standing;
+        }
+
+        if(toggleLight == true)
+        {
+            SpriteRenderer.sprite = laternSprite;
+        }
     }
 
     // Update is called once per frame
@@ -58,7 +106,17 @@ public class PlayerMovement : MonoBehaviour
         speed += sprintSpeed;
         isSprinting = true;
     }
-
+    //Enable Light
+    public void LightOn()
+    {
+        toggleLight = true;
+        latern.SetActive(true);
+    }
+    public void LightOff()
+    {
+        toggleLight = false;
+        latern.SetActive(false);
+    }
     //Jump Function
     public void Jump()
     {
