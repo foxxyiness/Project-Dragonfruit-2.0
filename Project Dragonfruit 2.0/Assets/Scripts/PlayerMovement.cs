9c28d647b50ba54636b821e67e0b451452e3859c
foxxyiness <coachfoxjp@gmail.com>
2022-01-31 16:24:20 -0500

    Build Stuff

--- Project Dragonfruit 2.0/Assets/Scripts/PlayerMovement.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/PlayerMovement.cs
@@ -29,71 +29,6 @@ public class PlayerMovement : MonoBehaviour
     public bool isSprinting;
     public bool toggleLight;
 
-   
-    public void PlayerMove()
-    {
-        Collider.size = StandingSize;
-        SpriteRenderer = GetComponent<SpriteRenderer>();
-        SpriteRenderer.sprite = standing;
-        movement = Input.GetAxis("Horizontal");
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
-        {
-            Jump();
-        }
-
-        if (Input.GetButton("Sprint") && (isSprinting == false && isGrounded == true))
-        {
-            Sprint();
-        }
-        else
-        {
-            speed = 200f;
-            isSprinting = false;
-        }
-
-        if (movement < 0.0f && faceRight == false)
-        {
-            FlipPlayer();
-        }
-        else if (movement > 0.0f && faceRight == true)
-        {
-            FlipPlayer();
-        }
-
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed * Time.deltaTime, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-
-        //Crouching
-        if(Input.GetButton("Crouch") && (isSprinting == false && isGrounded == true))
-        {
-            LightOff();
-            SpriteRenderer.sprite = crouching;
-            Collider.size = CrouchingSize;
-            isCrouching = true;
-        }
-        else
-        {
-            SpriteRenderer.sprite = standing;
-            Collider.size = StandingSize;
-            isCrouching = false;
-        } 
-
-        //Enabling Light
-        if(Input.GetButtonDown("Light") && (toggleLight == false && isCrouching == false))
-        {
-            LightOn();
-        }
-        else if(Input.GetButtonDown("Light") && (toggleLight == true && isCrouching == false))
-        {
-            LightOff();
-            SpriteRenderer.sprite = standing;
-        }
-
-        if(toggleLight == true)
-        {
-            SpriteRenderer.sprite = laternSprite;
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -131,6 +66,78 @@ public class PlayerMovement : MonoBehaviour
         Vector2 localScale = gameObject.transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
+    }
+
+    public void PlayerMove()
+    {
+        /***************************************************************************************************************************/
+        //Essential Movement
+        Collider.size = StandingSize;
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = standing;
+        movement = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+
+        if (Input.GetButton("Sprint") && (isSprinting == false && isGrounded == true))
+        {
+            Sprint();
+        }
+        else
+        {
+            speed = 200f;
+            isSprinting = false;
+        }
+
+        if (movement < 0.0f && faceRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (movement > 0.0f && faceRight == true)
+        {
+            FlipPlayer();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed * Time.deltaTime, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        /*********************************************************************************************************************************/
+
+        /********************************************************************************************************************************/
+        //Crouching
+        if (Input.GetButton("Crouch") && (isSprinting == false && isGrounded == true))
+        {
+            LightOff();
+            SpriteRenderer.sprite = crouching;
+            Collider.size = CrouchingSize;
+            isCrouching = true;
+        }
+        else
+        {
+            SpriteRenderer.sprite = standing;
+            Collider.size = StandingSize;
+            isCrouching = false;
+        }
+        /*******************************************************************************************************************************************/
+
+
+        /*************************************************************************************************************************************************/
+        //Enabling Light
+        if (Input.GetButtonDown("Light") && (toggleLight == false && isCrouching == false))
+        {
+            LightOn();
+        }
+        else if (Input.GetButtonDown("Light") && (toggleLight == true && isCrouching == false))
+        {
+            LightOff();
+            SpriteRenderer.sprite = standing;
+        }
+
+        if (toggleLight == true)
+        {
+            SpriteRenderer.sprite = laternSprite;
+        }
+        /****************************************************************************************************************************************************************/
     }
 
     //Collider for player to detect ground
