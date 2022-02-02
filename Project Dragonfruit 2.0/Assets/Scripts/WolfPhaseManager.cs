cbedf6a16152fef43da49bf6e6f52550f199f124
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2022-02-02 16:15:02 -0500

    AddedPhases

--- Project Dragonfruit 2.0/Assets/Scripts/WolfPhaseManager.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/WolfPhaseManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class WolfPhaseManager : MonoBehaviour
 {
+    public GameObject Phase1WolfEntity;
+    public GameObject Phase2WolfEntity;
     public GameObject Phase4WolfEntity;
+    public GameObject Phase3WolfEntity;
     public GameObject Phase4WolfSpawn;
     // Start is called before the first frame update
     void Start()
@@ -17,18 +20,21 @@ public class WolfPhaseManager : MonoBehaviour
     {
         if(HUDScript.Instance.stressLVL >=0 && HUDScript.Instance.stressLVL <= 25)
         {
-
+            Phase1WolfEntity.SetActive(true);
         }
         if (HUDScript.Instance.stressLVL > 25 && HUDScript.Instance.stressLVL >= 50)
         {
-
+            Phase1WolfEntity.SetActive(false);
+            Phase2WolfEntity.SetActive(true);
         }
         if (HUDScript.Instance.stressLVL > 50 && HUDScript.Instance.stressLVL >= 75)
         {
-
+            Phase2WolfEntity.SetActive(false);
+            Phase3WolfEntity.SetActive(true);
         }
         if (HUDScript.Instance.stressLVL > 75 && HUDScript.Instance.stressLVL >= 100)
         {
+            Phase3WolfEntity.SetActive(false);
             Instantiate(Phase4WolfEntity, Phase4WolfSpawn.Transform.Position, Quaternion.Identity);
         }
 
