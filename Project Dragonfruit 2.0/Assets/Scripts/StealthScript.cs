ed05cbdd6d16d79418f0bcb6d38566fff6b83030
RyguyRockz <78097100+RyguyRockz@users.noreply.github.com>
2022-02-07 14:57:23 -0500

    ok

---
+++ Project Dragonfruit 2.0/Assets/Scripts/StealthScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthScript : MonoBehaviour
+{
+    public bool isHidden = false;
+    public float resetval = 1.5f;
+    public float timer;
+    public static StealthScript Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        timer = resetval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHidden == false && resetval != 0f)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                
+            }
+            else
+            {
+                HUDScript.Instance.stressLVL += HUDScript.Instance.addAmount1;
+                timer = resetval;
+            }
+
+        }
+        else
+        {
+            timer = resetval;
+        }
+    }
+}
