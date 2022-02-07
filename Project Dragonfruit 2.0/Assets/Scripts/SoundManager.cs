6d56047d0e8eaa84b7488270a46bb423d8e57805
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2022-02-07 16:46:49 -0500

    Management Scripts and Trap Prefab Creation

---
+++ Project Dragonfruit 2.0/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource[] sounds;
+    public static SoundManager Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Project Dragonfruit 2.0/Assets/Scripts/TrapObject.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/TrapObject.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TrapObject : MonoBehaviour
 {
+    public float pointsadded;
+    private float timer = 0;
+    public float timedestroy = 2;
+    public bool activated = false;
+    public GameObject parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,26 @@ public class TrapObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (activated == true)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= timedestroy)
+        {
+            Destroy(parent);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            if (activated == false)
+            {
+                HUDScript.Instance.stressLVL += pointsadded;
+            }
+            activated = true;
+            Debug.Log("Trap Activated");
+        }
+
     }
 }
