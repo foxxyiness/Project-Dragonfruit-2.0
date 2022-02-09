a6cb05218f1e4096629f719b5cd0d8b2c8d9371b
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2022-02-09 16:14:41 -0500

    DummyFile

--- Project Dragonfruit 2.0/Assets/Scripts/WolfPhaseManager.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/WolfPhaseManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WolfPhaseManager : MonoBehaviour
 {
-    public GameObject Phase1WolfEntity;
     public GameObject Phase2WolfEntity;
     public GameObject Phase4WolfEntity;
     public GameObject Phase3WolfEntity;
@@ -20,26 +19,24 @@ public class WolfPhaseManager : MonoBehaviour
     {
         if(HUDScript.Instance.stressLVL >=0f && HUDScript.Instance.stressLVL <= 25f)
         {
-            //Phase1WolfEntity.SetActive(true);
             StealthScript.Instance.resetval = 0f;
 
         }
         if (HUDScript.Instance.stressLVL > 25f && HUDScript.Instance.stressLVL <= 50f)
         {
-            //Phase1WolfEntity.SetActive(false);
-            //Phase2WolfEntity.SetActive(true);
+            Phase2WolfEntity.SetActive(true);
             StealthScript.Instance.resetval = 1.5f;
         }
         if (HUDScript.Instance.stressLVL > 50f && HUDScript.Instance.stressLVL <= 75f)
         {
-            //Phase2WolfEntity.SetActive(false);
-            //Phase3WolfEntity.SetActive(true);
+            Phase2WolfEntity.SetActive(false);
+            Phase3WolfEntity.SetActive(true);
             StealthScript.Instance.resetval = 1f;
         }
         if (HUDScript.Instance.stressLVL > 75f && HUDScript.Instance.stressLVL <= 100f)
         {
-            //Phase3WolfEntity.SetActive(false);
-            //Instantiate(Phase4WolfEntity, Phase4WolfSpawn.transform.position, Quaternion.identity);
+            Phase3WolfEntity.SetActive(false);
+            Instantiate(Phase4WolfEntity, Phase4WolfSpawn.transform.position, Quaternion.identity);
             StealthScript.Instance.resetval = 0.5f;
         }
 
