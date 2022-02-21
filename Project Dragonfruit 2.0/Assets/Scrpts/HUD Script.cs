0d4a2d263f83fd455f4ddf0f47634b4287d103ae
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2022-02-21 15:46:51 -0500

    Edit

---
+++ Project Dragonfruit 2.0/Assets/Scrpts/HUD Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDScript : MonoBehaviour
+{
+    public float stressLVL = 0;
+    public float addAmount1;
+    public static HUDScript Instance;
+    public TextMeshProUGUI Display1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Display1.text = stressLVL.ToString();
+    }
+}
