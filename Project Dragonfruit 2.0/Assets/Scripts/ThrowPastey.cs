212a1231990817f1bfe817ac84a789966ed5c9e1
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2022-02-23 15:31:32 -0500

    Pastry Add

--- Project Dragonfruit 2.0/Assets/Scripts/ThrowPastey.cs
+++ Project Dragonfruit 2.0/Assets/Scripts/ThrowPastey.cs
@@ -7,22 +7,31 @@ public class ThrowPastey : MonoBehaviour
     public float Speed = 4;
     public Vector3 LaunchOffset;
     public bool Thrown;
+    public Rigidbody2D RB2D;
 
     void Start()
     {
+        RB2D = GetComponent<Rigidbody2D>();
         if (Thrown && (PlayerMovement.faceRight == true))
         {
             var direction = -transform.right + Vector3.up;
-            GetComponent<Rigidbody2D>().AddForce(direction * Speed, ForceMode2D.Impulse);
+            RB2D.AddForce(direction * Speed, ForceMode2D.Impulse);
         }
         else if (Thrown && (PlayerMovement.faceRight == false))
         {
             var direction = transform.right + Vector3.up;
-            GetComponent<Rigidbody2D>().AddForce(direction * Speed, ForceMode2D.Impulse);
+            RB2D.AddForce(direction * Speed, ForceMode2D.Impulse);
         }
     }
 
-
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag =="Ground")
+        {
+            RB2D.constraints = RigidbodyConstraints2D.FreezePositionX;
+            RB2D.constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+    }
 
 
     // Update is called once per frame
